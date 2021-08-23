d7f4994b72e7d3857900bffc098f1182e29dae74
Hyungki Kim <diskhkme@gmail.com>
2021-08-24 01:36:12 +0900

    surface normals and multiple objects

--- lessons/_current/raytracingFragment.js
+++ lessons/_current/raytracingFragment.js
@@ -6,25 +6,102 @@ layout(location=0) out vec4 outColor;
 
 uniform vec2 u_resolution;
 
-//---
+//--- Scene
+const int SPHERE_COUNT = 2;
+
+struct sphere {
+    vec3 center;
+    float radius;
+};
+
+sphere sceneList[] = sphere[SPHERE_COUNT](
+sphere(vec3( 0,0,-1), 0.5),
+sphere(vec3( 0,-100.5,-1), 100.0)
+);
+
+//--- Ray & Hit Record
 struct ray {
     vec3 orig;
     vec3 dir;
 };
-
-bool hit_sphere(const vec3 center, float radius, const ray r) {
-    vec3 oc = r.orig - center;
-    float a = dot(r.dir, r.dir);
-    float b = 2.0 * dot(oc, r.dir);
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - 4.0*a*c;
-    return (discriminant > 0.0);
+vec3 ray_at(ray r, float t)
+{
+    return r.orig + t*r.dir;
 }
 
-vec3 ray_color(const ray r)
+struct hit_record {
+    vec3 p;
+    vec3 normal;
+    float t;
+    bool front_face;
+};
+void set_face_normal(out hit_record rec, ray r, vec3 outward_normal)
 {
-    if(hit_sphere(vec3(0,0,-1), 0.5, r))
-        return vec3(1,0,0);
+    rec.front_face = dot(r.dir, outward_normal) < 0.0;
+    rec.normal = rec.front_face ? outward_normal : -outward_normal;
+}
+
+//--- Sphere Hit Test
+bool hit_sphere(sphere s, ray r, float t_min, float t_max, out hit_record rec) {
+    vec3 oc = r.orig - s.center;
+    float a = dot(r.dir, r.dir);
+    float half_b = dot(oc, r.dir);
+    float c = dot(oc, oc) - s.radius*s.radius;
+    float discriminant = half_b*half_b - a*c;
+    
+    if(discriminant < 0.0)
+    {
+        return false;
+    }
+    float sqrtd = sqrt(discriminant);
+    
+    float root = (-half_b - sqrtd) / a;
+    if(root < t_min || root > t_max)
+    {
+        root = (-half_b + sqrtd) / a;
+        if(root < t_min || root > t_max)
+        {
+            return false;
+        }
+    }
+
+    rec.t = root;
+    rec.p = ray_at(r, rec.t);
+    vec3 outward_normal = (rec.p - s.center)/s.radius;
+    set_face_normal(rec, r, outward_normal);
+
+    return true;
+}
+
+//--- Scene Hit Test
+bool hit_scene(ray r, float t_min, float t_max, out hit_record rec)
+{
+    hit_record temp_rec;
+    bool hit_anything = false;
+    float closest_so_far = t_max;
+
+    for (int i=0;i<SPHERE_COUNT;i++)
+    {
+        if(hit_sphere(sceneList[i], r, t_min, t_max, temp_rec) && temp_rec.t < closest_so_far)
+        {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+
+    return hit_anything;
+}
+
+//---
+
+vec3 ray_color(ray r)
+{
+    hit_record rec;
+    if(hit_scene(r, 0.0, 10000.0, rec))
+    {
+        return 0.5 * (rec.normal + vec3(1,1,1));
+    }
 
     vec3 unit_direction = normalize(r.dir);
     float t = 0.5*(unit_direction.y + 1.0);
