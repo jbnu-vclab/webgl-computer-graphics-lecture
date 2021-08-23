d136ca6b5705d3c445735822d05c9c33cfbe9511
Hyungki Kim <diskhkme@gmail.com>
2021-08-24 00:40:41 +0900

    adding a sphere

--- lessons/_current/raytracingFragment.js
+++ lessons/_current/raytracingFragment.js
@@ -12,8 +12,20 @@ struct ray {
     vec3 dir;
 };
 
+bool hit_sphere(const vec3 center, float radius, const ray r) {
+    vec3 oc = r.orig - center;
+    float a = dot(r.dir, r.dir);
+    float b = 2.0 * dot(oc, r.dir);
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4.0*a*c;
+    return (discriminant > 0.0);
+}
+
 vec3 ray_color(const ray r)
 {
+    if(hit_sphere(vec3(0,0,-1), 0.5, r))
+        return vec3(1,0,0);
+
     vec3 unit_direction = normalize(r.dir);
     float t = 0.5*(unit_direction.y + 1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
