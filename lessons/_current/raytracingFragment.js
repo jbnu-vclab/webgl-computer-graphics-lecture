52ac703f046524f1028ba42f36caa7d6db7d280d
Hyungki Kim <diskhkme@gmail.com>
2021-08-24 00:23:03 +0900

    rays, a simple camera, and background

--- lessons/_current/raytracingFragment.js
+++ lessons/_current/raytracingFragment.js
@@ -6,7 +6,37 @@ layout(location=0) out vec4 outColor;
 
 uniform vec2 u_resolution;
 
+//---
+struct ray {
+    vec3 orig;
+    vec3 dir;
+};
+
+vec3 ray_color(const ray r)
+{
+    vec3 unit_direction = normalize(r.dir);
+    float t = 0.5*(unit_direction.y + 1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
 void main() {
-    outColor = vec4((gl_FragCoord.xy / u_resolution), 0, 1);
+    float aspect_ratio = u_resolution.x / u_resolution.y;
+    float viewport_height = 2.0;
+    float viewport_width = aspect_ratio * viewport_height;
+    float focal_length = 1.0;
+
+    vec3 origin = vec3(0,0,0);
+    vec3 horizontal = vec3(viewport_width, 0, 0);
+    vec3 vertical = vec3(0, viewport_height, 0);
+    vec3 lower_left_corner = origin - horizontal*0.5 - vertical*0.5 - vec3(0,0,focal_length);
+
+    float u = gl_FragCoord.x / u_resolution.x;
+    float v = gl_FragCoord.y / u_resolution.y;
+
+    ray r;
+    r.orig = origin;
+    r.dir = lower_left_corner + u*horizontal + v*vertical - origin;
+
+    outColor = vec4(ray_color(r),1.0);
 }
 `;
